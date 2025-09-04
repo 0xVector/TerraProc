610553214d50edee014baed8ed129f2be7b9f649
0xVector <hello@0xvector.me>
2025-09-04 20:24:29 +0200

    Core: Add SimpleNoiseTerrainGenerator

---
+++ src/TerraProc.Core/Generation/SimpleNoiseTerrainGenerator.cs
@@ -0,0 +1,35 @@
+using TerraProc.Core.Noise;
+using TerraProc.Core.Terrain;
+
+namespace TerraProc.Core.Generation;
+
+/// <summary>
+/// Simple terrain generator that samples a single noise function.
+/// It does no octave layering or other complex operations.
+/// </summary>
+/// <param name="noiseFactory">Factory function to create a noise provider given a seed.</param>
+/// <param name="seed">Main seed for the terrain generation.</param>
+public class SimpleNoiseTerrainGenerator(NoiseProviderFactory noiseFactory, Seed seed) : ITerrainGenerator
+{
+    private readonly INoiseProvider _noise = noiseFactory(seed);
+
+    public ChunkData Generate(ChunkCoords coords)
+    {
+        var heights = new Height[GridLayout.ChunkTileCount];
+        var materials = new Material[GridLayout.ChunkTileCount];
+
+        var (globX, globY) = coords.ToTileCoords();
+
+        for (var i = 0; i < GridLayout.ChunkTileCount; i++)
+        {
+            var (yOff, xOff) = Math.DivRem(i, GridLayout.ChunkSize); // TODO: add (de)linearize public API 
+            var tileX = globX + xOff + .5; // Center of tile
+            var tileY = globY + yOff + .5;
+            var n = _noise.Sample(tileX, tileY);
+            heights[i] = (Height)(n * GridLayout.MaxHeight);
+            materials[i] = Material.Default;
+        }
+
+        return ChunkData.FromOwned(heights, materials);
+    }
+}
